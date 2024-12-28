5b1bb8ab9106586b551284dc7392ef1d592651df
rahmadahyaWeb <thelastpc24@gmail.com>
2024-12-28 19:37:10 +0800

    nested component

---
+++ src/hello-world/Container.jsx
@@ -0,0 +1,11 @@
+export default function Container({ children }) {
+  return (
+    <div>
+      <h1>Rahmadahya</h1>
+      {children}
+      <footer>
+        <p>2024 Rahmadahya</p>
+      </footer>
+    </div>
+  );
+}
