a2d6ff8c30da1e7efda5b1d5566e8861b0261e0b
rahmadahyaWeb <thelastpc24@gmail.com>
2024-12-29 01:39:35 +0800

    event handler

--- src/todolist/main.jsx
+++ src/todolist/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import TodoList from "./TodoList";
 import Container from "../Container";
+import AlertButton from "../button/AlertButton";
+import MyButton from "../button/MyButton";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Container>
       <TodoList />
+      <AlertButton text="Click me" message="ouch..." />
+      <MyButton text="Smash me" onSmash={() => alert("Ouch... u smash me")} />
     </Container>
   </StrictMode>
 );
