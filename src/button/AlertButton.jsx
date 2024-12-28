2cd9a1735c98087952cd1d5ebdd1d7a41be2a19d
rahmadahyaWeb <thelastpc24@gmail.com>
2024-12-29 01:41:58 +0800

    event object

--- src/button/AlertButton.jsx
+++ src/button/AlertButton.jsx
@@ -1,5 +1,6 @@
-export default function AlertButton({text, message}) {
-  function handleClick() {
+export default function AlertButton({ text, message }) {
+  function handleClick(e) {
+    console.info(e);
     alert(message);
   }
 
