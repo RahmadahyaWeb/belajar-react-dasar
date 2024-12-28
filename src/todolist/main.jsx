b011e799d7b32755f54ebb0f780a0ed3ebcf1ecf
rahmadahyaWeb <thelastpc24@gmail.com>
2024-12-29 00:51:43 +0800

    todolist conditional component

--- src/todolist/main.jsx
+++ src/todolist/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import HelloWorld from "./HelloWorld";
+import TodoList from "./TodoList";
 import Container from "../Container";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Container>
-      <HelloWorld />
+      <TodoList />
     </Container>
   </StrictMode>
 );
