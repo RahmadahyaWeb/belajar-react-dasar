2dbda05a675288ca106eb205c612b7de57fb4260
rahmadahyaWeb <thelastpc24@gmail.com>
2024-12-30 10:11:32 +0800

    sharing state

--- src/task/Task.jsx
+++ src/task/Task.jsx
@@ -1,42 +1,14 @@
-import { useState } from "react";
 import { useImmer } from "use-immer";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
 
 export default function Task() {
-  const [item, setItem] = useState("");
-
   const [items, setItems] = useImmer([]);
-
-  function handleChange(e) {
-    setItem(e.target.value);
-  }
-
-  function handleClick(e) {
-    e.preventDefault();
-    setItems((draft) => {
-      draft.push(item);
-    });
-
-    resetItem();
-  }
-
-  function resetItem() {
-    setItem("");
-  }
 
   return (
     <div>
-      <h1>Create Task</h1>
-      <form>
-        <input type="text" value={item} onChange={handleChange} />
-        <button onClick={handleClick}>Add</button>
-      </form>
-
-      <h1>List Task</h1>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <TaskForm setItems={setItems} />
+      <TaskList items={items} />
     </div>
   );
 }
